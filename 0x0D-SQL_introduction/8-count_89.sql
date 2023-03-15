1dba6e9c1b180647aa095e2281de852212795c53
nmaEbube <oluebubeonyemaobim@gmail.com>
2023-03-15 13:21:40 -0700

    sore

---
+++ 0x0D-SQL_introduction/8-count_89.sql
@@ -0,0 +1,8 @@
+-- Displays the number of records with id = 89 in the table first_table.
+SELECT COUNT(*)
+FROM `first_table`
+WHERE `id` = 89;
+Footer
+© 2023 GitHub, Inc.
+Footer navigation
+Terms
