5356a119a7c173eaa3b29a8f07e7bc3630d13a46
nmaEbube <oluebubeonyemaobim@gmail.com>
2023-03-15 20:28:27 -0700

    tatar

---
+++ 0x0E-SQL_more_queries/3-force_name.sql
@@ -0,0 +1,11 @@
+-- Creates the table force_name.
+CREATE TABLE IF NOT EXISTS `force_name` (
+    `id`   INT,
+    `name` VARCHAR(256) NOT NULL
+);
+Footer
+© 2023 GitHub, Inc.
+Footer navigation
+Terms
+Privacy
+Security
